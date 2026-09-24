c61e18572afeebd4d849068f9f9897992aee511f
Daimon <260491108+daimonbot@users.noreply.github.com>
2026-09-24 11:55:22 +0200

    chore: remove local preview configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'dist', emptyOutDir: true },
-  server: {
-    allowedHosts: ['astra.dev.rubasace.dev'],
-    proxy: { '/api': 'http://127.0.0.1:4749', '/healthz': 'http://127.0.0.1:4749' },
-  },
-});
+export default defineConfig({ plugins: [react()], build: { outDir: 'dist', emptyOutDir: true } });
